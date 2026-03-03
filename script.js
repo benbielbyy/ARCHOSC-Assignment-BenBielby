1ff84969428734bcf66d932c14b4bfbfe4c2f27d
benbielbyy <b.bielby-2024@hull.ac.uk>
2026-03-03 12:07:12 +0000

    readded all lab4 stuff that i forgot to push in the lab

---
+++ script.js
@@ -0,0 +1,9 @@
+const myName = document.querySelector(".name");
+const description = document.querySelector(".description");
+console.log(myName);
+console.log(description); 
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
